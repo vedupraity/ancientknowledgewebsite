936ee8c3a9d9eb214b4c5e383a1d09bf619164a0
Vedprakash Upraity <vedprakashupraity@gmail.com>
2021-04-14 01:19:52 +0530

    new static pages

--- assets/js/constants.js
+++
@@ -1,9 +0,0 @@
-/* begin dev urls */
-// siteUrlRoot = "http://localhost"
-// dbUrlRoot = "http://localhost/db-local"
-/* end dev urls */
-
-/* begin live urls */
-siteUrlRoot = "https://www.ancientknowledge.in";
-dbUrlRoot = "https://vedupraity.github.io/ancientknowledgedatabase"
-/* end live urls */
