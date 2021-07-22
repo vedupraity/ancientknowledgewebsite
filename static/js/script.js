1734ae6aa9d752fad22afd0bd0f6718a567a7032
Vedprakash Upraity <vedprakashupraity@gmail.com>
2021-07-23 00:11:31 +0530

    New Theme

---
+++ static/js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Begin 🍔
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+    // End 🍔
+
+    // Begin 🔔
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        const $notification = $delete.parentNode;
+
+        $delete.addEventListener('click', () => {
+            $notification.parentNode.removeChild($notification);
+        });
+    });
+    // End 🔔
+
+    // Begin 📜
+    window.scrollToCards = () => {
+        const cardsSection = document.getElementById('cards-section');
+
+        // scrollTo(0, cardsSection.offsetTop);
+        scrollTo({
+            top: cardsSection.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+    // End 📜
+});
